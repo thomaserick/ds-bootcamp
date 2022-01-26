233fecb2e443f9aa3b5d1699fd00b57767bfe205
Thomas Erick <thomaserick.dev@gmail.com>
2022-01-25 22:26:04 -0300

    Ajustes pastas

--- src/main/java/com/tef/dscatalog/repositories/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tef.dscatalog.repositories;
-
-import com.tef.dscatalog.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long>
-{
-}
