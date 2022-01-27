906fe5f01e7b745e10bf75e403519f1ba9dab03a
Thomas Erick <thomaserick.dev@gmail.com>
2022-01-27 12:41:35 -0300

    Tests Junit5 - Factory

--- aula-junit/src/test/java/entities/AccountTests.java
+++ aula-junit/src/test/java/entities/AccountTests.java
@@ -1,5 +1,6 @@
 package entities;
 
+import factory.AccountFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,13 +13,24 @@ public class AccountTests
 		//Arrange
 		double amount = 200.0;
 		double expectedValue = 196.0;
-		Account account = new Account(1L,0.0);
+		Account account = AccountFactory.createEmptyAccount();
 
 		//Action
 		account.deposit(amount);
 
 		//Assert
-		Assertions.assertEquals(expectedValue,account.getBalance());
+		Assertions.assertEquals(expectedValue, account.getBalance());
+	}
 
+	@Test
+	public void depositShouldDoNothingWhenNegativeAmount()
+	{
+		double expectedValue = 100.00;
+		Account account = AccountFactory.createAccount(expectedValue);
+
+		double amount = -200.00;
+		account.deposit(amount);
+
+		Assertions.assertEquals(expectedValue, account.getBalance());
 	}
 }
